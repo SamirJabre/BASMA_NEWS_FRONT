9d3f8ab1c639ecee5636d3904d93ffa925a1606c
Samir Jabre <samirjabre8@gmail.com>
2025-01-19 14:42:38 +0200

    Add axios dependency and implement dynamic category fetching in NewsCategories component

--- src/Components/CategoryBox/CategoryBox.jsx
+++ src/Components/CategoryBox/CategoryBox.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import sport from "../../Assets/Images/828.png";
 
-function CategoryBox() {
+function CategoryBox({ category }) {
   return (
     <div className=" w-40 h-60 rounded">
       <div className="w-full h-36">
-        <img src={sport} alt="Sport Image" className="w-full h-full rounded-t" />
+        <img
+          src={category.image}
+          alt="Category Image"
+          className="w-full h-full rounded-t"
+        />
       </div>
       <div className="w-full h-1/5 flex justify-between items-center px-4 border-x-2 border-b-2 rounded-b">
         <button className="rounded-full px-2 py-1 text-white bg-[#34B190] text-xs text-center">
           المزيد
         </button>
-        <h1 className="font-semibold text-xl text-[#EA543F]">رياضة</h1>
+        <h1 className="font-semibold text-xl text-[#EA543F]">
+          {category.title}
+        </h1>
       </div>
     </div>
   );
--- src/Components/NewsCatergories/NewsCategories.jsx
+++ src/Components/NewsCatergories/NewsCategories.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CategoryBox from "../CategoryBox/CategoryBox";
+import axios from "axios";
 
 function NewsCategories() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://192.168.1.108:8000/api/get_category").then((Response) => {
+      const data = Response.data;
+      setCategories(data.sort((a, b) => b.clicks - a.clicks));
+    });
+  }, []);
+
   return (
     <section className="bg-gray-100 w-full h-fit py-7 flex justify-evenly items-center flex-wrap">
-      <CategoryBox/>
-      <CategoryBox/>
-      <CategoryBox/>
-      <CategoryBox/>
-      <CategoryBox/>
-      <CategoryBox/>
+      {categories.map((category) => {
+        return <CategoryBox key={category.id} category={category} />;
+      })}
     </section>
   );
 }
