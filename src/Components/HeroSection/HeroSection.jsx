7304c27242e5a9d95cdc44ad24d038d455b1d29c
Samir Jabre <samirjabre8@gmail.com>
2025-01-27 23:56:59 +0200

    Add language support to HeroSection component; implement dynamic text rendering based on selected language

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -3,9 +3,11 @@ import newsBackground from "../../Assets/Images/NewsBack.jpg";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 import Navbar from "../Navbar/Navbar";
+import { useSelector } from "react-redux";
 
 function HeroSection() {
   const [width, setWidth] = useState(window.innerWidth);
+  const language = useSelector((state) => state.auth.language);
   window.addEventListener("resize", () => {
     setWidth(window.innerWidth);
   });
@@ -43,30 +45,24 @@ function HeroSection() {
           {width > 640 ? (
             <>
               <Navbar />
-              <div className="w-full sm:px-7 md:px-10 lg:px-20 xl:px-28 2xl:px-32 flex flex-col sm:items-end sm:justify-between">
+              <div className={`w-full sm:px-7 md:px-10 lg:px-20 xl:px-28 2xl:px-32 flex flex-col ${language==='ar' ? 'sm:items-end' : 'sm:items-start'} sm:justify-between`}>
                 <div className="w-fit h-fit pb-2 pl-4 border-b">
                 <h1 className="font-messiri text-white font-extrabold sm:text-[15px] md:text-[20px] lg:text-[30px] xl:text-[35px] 2xl:text-[45px] ">
-                  خلافا للاعتقاد
+                  {language==='ar'? 'خلافا للاعتقاد' : 'Contrary to belief'}
                 </h1>
                 </div>
-                <h1 className=" sm:text-base lg:text-lg xl:text-xl 2xl:text-2xl font-normal text-right my-5 text-white w-full">
-                  لوريم ايبسوم ليس نصا عشوائيا, بل له جذور في الادب الاتيني
-                  الكلاسيكي منذ العام 45 قبل الميلاد, مما يجعله اكثر من 2000 عام
-                  في القدم لوريم ايبسوم ليس نصا عشوائيا, بل له جذور في الادب
-                  الاتيني الكلاسيكي منذ العام 45 قبل الميلاد, مما يجعله اكثر من
-                  2000 عام في القدم لوريم ايبسوم ليس نصا عشوائيا, بل له جذور في
-                  الادب الاتيني الكلاسيكي منذ العام 45 قبل الميلاد, مما يجعله
-                  اكثر من 2000 عام في القدم
+                <h1 className={`sm:text-base lg:text-lg xl:text-xl 2xl:text-2xl font-normal ${language==='ar' ? 'text-right' : 'text-left'} my-5 text-white w-full`}>
+                  {language==='ar'? 'لوريم ايبسوم ليس نصا عشوائيا, بل له جذور في الادب الاتيني الكلاسيكي منذ العام 45 قبل الميلاد, مما يجعله اكثر من 2000 عام في القدم لوريم ايبسوم ليس نصا عشوائيا, بل له جذور في الادب الاتيني الكلاسيكي منذ العام 45 قبل الميلاد, مما يجعله اكثر من 2000 عام في القدم لوريم ايبسوم ليس نصا عشوائيا, بل له جذور في الادب الاتيني الكلاسيكي منذ العام 45 قبل الميلاد, مما يجعله اكثر من 2000 عام في القدم' : 'Lorem Ipsum is not random text, but has roots in classical Latin literature since 45 BC, making it more than 2000 years old. Lorem Ipsum is not random text, but has roots in classical Latin literature since 45 BC, making it more than 2000 years old. Lorem Ipsum is not random text, but has roots in classical Latin literature since 45 BC, making it more than 2000 years old.'}
                 </h1>
                 <button className="bg-[#E64128] rounded-full px-4 py-1 text-center text-base text-white hover:text-[#E64128] hover:bg-white hover:scale-110 hover:transition-all">
-                  المزيد
+                  {language==='ar'? 'المزيد' : 'More'}
                 </button>
               </div>
             </>
           ) : (
             <div className="w-full h-fit px-5 flex flex-col justify-evenly items-end">
               <div className="w-fit pl-2 h-7 border-b">
-                <h1 className="font-messiri text-white font-bold">خلافا للاعتقاد</h1>
+                <h1 className="font-messiri text-white font-extrabold sm:text-[15px] md:text-[20px] lg:text-[30px] xl:text-[35px] 2xl:text-[45px]">خلافا للاعتقاد</h1>
               </div>
               <div className="h-fit w-full flex justify-between items-center">
                 <button className="bg-[#E64128] rounded-full px-2 text-sm text-white">
@@ -81,34 +77,28 @@ function HeroSection() {
             </div>
           )}
         </div>
-        <div className="bg-newsBackground bg-no-repeat bg-cover w-full h-[250px] py-12 flex items-end justify-center sm:h-[800px] sm:flex sm:flex-col sm:items-center sm:justify-between sm:py-0 sm:pb-10">
+        <div className="bg-background2 bg-no-repeat bg-cover w-full h-[250px] py-12 flex items-end justify-center sm:h-[800px] sm:flex sm:flex-col sm:items-center sm:justify-between sm:py-0 sm:pb-10">
           {width > 640 ? (
             <>
               <Navbar />
-              <div className="w-full px-32 flex flex-col sm:items-end sm:justify-between">
-                <div className="w-fit h-fit pb-2 border-b">
+              <div className={`w-full sm:px-7 md:px-10 lg:px-20 xl:px-28 2xl:px-32 flex flex-col ${language==='ar' ? 'sm:items-end' : 'sm:items-start'} sm:justify-between`}>
+                <div className="w-fit h-fit pb-2 pl-4 border-b">
                 <h1 className="font-messiri text-white font-extrabold sm:text-[15px] md:text-[20px] lg:text-[30px] xl:text-[35px] 2xl:text-[45px] ">
-                  خلافا للاعتقاد
+                  {language==='ar'? 'خلافا للاعتقاد' : 'Contrary to belief'}
                 </h1>
                 </div>
-                <h1 className="sm:text-base lg:text-lg xl:text-xl 2xl:text-2xl font-semibold text-right my-5 text-white w-full">
-                  لوريم ايبسوم ليس نصا عشوائيا, بل له جذور في الادب الاتيني
-                  الكلاسيكي منذ العام 45 قبل الميلاد, مما يجعله اكثر من 2000 عام
-                  في القدم لوريم ايبسوم ليس نصا عشوائيا, بل له جذور في الادب
-                  الاتيني الكلاسيكي منذ العام 45 قبل الميلاد, مما يجعله اكثر من
-                  2000 عام في القدم لوريم ايبسوم ليس نصا عشوائيا, بل له جذور في
-                  الادب الاتيني الكلاسيكي منذ العام 45 قبل الميلاد, مما يجعله
-                  اكثر من 2000 عام في القدم
+                <h1 className={`sm:text-base lg:text-lg xl:text-xl 2xl:text-2xl font-normal ${language==='ar' ? 'text-right' : 'text-left'} my-5 text-white w-full`}>
+                  {language==='ar'? 'لوريم ايبسوم ليس نصا عشوائيا, بل له جذور في الادب الاتيني الكلاسيكي منذ العام 45 قبل الميلاد, مما يجعله اكثر من 2000 عام في القدم لوريم ايبسوم ليس نصا عشوائيا, بل له جذور في الادب الاتيني الكلاسيكي منذ العام 45 قبل الميلاد, مما يجعله اكثر من 2000 عام في القدم لوريم ايبسوم ليس نصا عشوائيا, بل له جذور في الادب الاتيني الكلاسيكي منذ العام 45 قبل الميلاد, مما يجعله اكثر من 2000 عام في القدم' : 'Lorem Ipsum is not random text, but has roots in classical Latin literature since 45 BC, making it more than 2000 years old. Lorem Ipsum is not random text, but has roots in classical Latin literature since 45 BC, making it more than 2000 years old. Lorem Ipsum is not random text, but has roots in classical Latin literature since 45 BC, making it more than 2000 years old.'}
                 </h1>
                 <button className="bg-[#E64128] rounded-full px-4 py-1 text-center text-base text-white hover:text-[#E64128] hover:bg-white hover:scale-110 hover:transition-all">
-                  المزيد
+                  {language==='ar'? 'المزيد' : 'More'}
                 </button>
               </div>
             </>
           ) : (
             <div className="w-full h-fit px-5 flex flex-col justify-evenly items-end">
               <div className="w-fit pl-2 h-7 border-b">
-                <h1 className="text-white font-bold">خلافا للاعتقاد</h1>
+                <h1 className="font-messiri text-white font-extrabold sm:text-[15px] md:text-[20px] lg:text-[30px] xl:text-[35px] 2xl:text-[45px]">خلافا للاعتقاد</h1>
               </div>
               <div className="h-fit w-full flex justify-between items-center">
                 <button className="bg-[#E64128] rounded-full px-2 text-sm text-white">
@@ -123,34 +113,28 @@ function HeroSection() {
             </div>
           )}
         </div>
-        <div className="bg-newsBackground bg-no-repeat bg-cover w-full h-[250px] py-12 flex items-end justify-center sm:h-[800px] sm:flex sm:flex-col sm:items-center sm:justify-between sm:py-0 sm:pb-10">
+        <div className="bg-background2 bg-no-repeat bg-cover w-full h-[250px] py-12 flex items-end justify-center sm:h-[800px] sm:flex sm:flex-col sm:items-center sm:justify-between sm:py-0 sm:pb-10">
           {width > 640 ? (
             <>
               <Navbar />
-              <div className="w-full px-32 flex flex-col sm:items-end sm:justify-between">
-                <div className="w-fit h-fit pb-2 border-b">
-                <h1 className="text-white font-extrabold sm:text-[15px] md:text-[20px] lg:text-[30px] xl:text-[35px] 2xl:text-[45px] ">
-                  خلافا للاعتقاد
+              <div className={`w-full sm:px-7 md:px-10 lg:px-20 xl:px-28 2xl:px-32 flex flex-col ${language==='ar' ? 'sm:items-end' : 'sm:items-start'} sm:justify-between`}>
+                <div className="w-fit h-fit pb-2 pl-4 border-b">
+                <h1 className="font-messiri text-white font-extrabold sm:text-[15px] md:text-[20px] lg:text-[30px] xl:text-[35px] 2xl:text-[45px] ">
+                  {language==='ar'? 'خلافا للاعتقاد' : 'Contrary to belief'}
                 </h1>
                 </div>
-                <h1 className="sm:text-base lg:text-lg xl:text-xl 2xl:text-2xl font-semibold text-right my-5 text-white w-full">
-                  لوريم ايبسوم ليس نصا عشوائيا, بل له جذور في الادب الاتيني
-                  الكلاسيكي منذ العام 45 قبل الميلاد, مما يجعله اكثر من 2000 عام
-                  في القدم لوريم ايبسوم ليس نصا عشوائيا, بل له جذور في الادب
-                  الاتيني الكلاسيكي منذ العام 45 قبل الميلاد, مما يجعله اكثر من
-                  2000 عام في القدم لوريم ايبسوم ليس نصا عشوائيا, بل له جذور في
-                  الادب الاتيني الكلاسيكي منذ العام 45 قبل الميلاد, مما يجعله
-                  اكثر من 2000 عام في القدم
+                <h1 className={`sm:text-base lg:text-lg xl:text-xl 2xl:text-2xl font-normal ${language==='ar' ? 'text-right' : 'text-left'} my-5 text-white w-full`}>
+                  {language==='ar'? 'لوريم ايبسوم ليس نصا عشوائيا, بل له جذور في الادب الاتيني الكلاسيكي منذ العام 45 قبل الميلاد, مما يجعله اكثر من 2000 عام في القدم لوريم ايبسوم ليس نصا عشوائيا, بل له جذور في الادب الاتيني الكلاسيكي منذ العام 45 قبل الميلاد, مما يجعله اكثر من 2000 عام في القدم لوريم ايبسوم ليس نصا عشوائيا, بل له جذور في الادب الاتيني الكلاسيكي منذ العام 45 قبل الميلاد, مما يجعله اكثر من 2000 عام في القدم' : 'Lorem Ipsum is not random text, but has roots in classical Latin literature since 45 BC, making it more than 2000 years old. Lorem Ipsum is not random text, but has roots in classical Latin literature since 45 BC, making it more than 2000 years old. Lorem Ipsum is not random text, but has roots in classical Latin literature since 45 BC, making it more than 2000 years old.'}
                 </h1>
                 <button className="bg-[#E64128] rounded-full px-4 py-1 text-center text-base text-white hover:text-[#E64128] hover:bg-white hover:scale-110 hover:transition-all">
-                  المزيد
+                  {language==='ar'? 'المزيد' : 'More'}
                 </button>
               </div>
             </>
           ) : (
             <div className="w-full h-fit px-5 flex flex-col justify-evenly items-end">
               <div className="w-fit pl-2 h-7 border-b">
-                <h1 className="text-white font-bold">خلافا للاعتقاد</h1>
+                <h1 className="font-messiri text-white font-extrabold sm:text-[15px] md:text-[20px] lg:text-[30px] xl:text-[35px] 2xl:text-[45px]">خلافا للاعتقاد</h1>
               </div>
               <div className="h-fit w-full flex justify-between items-center">
                 <button className="bg-[#E64128] rounded-full px-2 text-sm text-white">
